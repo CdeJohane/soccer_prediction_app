834f9f4519d795d7fdc5eef7ca1ab8236ceb3242
CdeJohane <j.r.t.johane@gmail.com>
2024-08-09 15:05:14 +0200

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import './pages/login.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    title: 'Soccer Predicitons',
+    home: Login()
+  ));
+}
+
+
