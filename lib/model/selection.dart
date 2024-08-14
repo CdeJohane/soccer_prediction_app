9d9c02173424373c5a3344884d239a357c6767e7
CdeJohane <j.r.t.johane@gmail.com>
2024-08-14 09:03:37 +0200

    Selection added

---
+++ lib/model/selection.dart
@@ -0,0 +1,7 @@
+class Selection{
+  final int selectionID;
+  final int playerID;
+  final int predictionID;
+
+  Selection({required this.selectionID, required this.playerID, required this.predictionID});
+}
