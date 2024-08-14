b2919d5ae74fe58fe09d4c74c91b2109fc0022d1
CdeJohane <j.r.t.johane@gmail.com>
2024-08-14 08:58:44 +0200

    Added match Class

---
+++ lib/model/match.dart
@@ -0,0 +1,34 @@
+class Match{
+  // Variables to be used
+  int? _matchID;
+  int? _homeID;
+  int? _awayID;
+  bool _completed = false;
+  int _homeScore = 0;
+  int _awayScore = 0;
+  int _winnerID = -1;
+  DateTime? _date;
+
+  // Use Default COnstructor
+  Match(this._matchID, this._homeID, this._awayID, this._date, this._completed, this._homeScore, this._awayScore, this._winnerID);
+
+  // Functions for winner
+  void setWinner(int hScore, int aScore){
+    if (_completed){
+      // Set home and Away Scores
+      _homeScore = hScore;
+      _awayScore = aScore;
+
+      // Compare the 2 scores
+      int diff = hScore - aScore;
+
+      if (diff > 0){
+        _winnerID = _homeID!; // Home Win
+      } else if (diff < 0){
+        _winnerID = _awayID!; // Away Win
+      } else {
+        _winnerID = 0;// Indicates draw
+      }
+    }
+  }
+}
