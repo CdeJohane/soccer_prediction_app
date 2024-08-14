4fa267870f181dadd468e4784fb6ae0a05050962
CdeJohane <j.r.t.johane@gmail.com>
2024-08-14 10:46:03 +0200

    Added Player Class

---
+++ lib/model/player.dart
@@ -0,0 +1,36 @@
+import 'package:soccer_app/model/selection.dart';
+
+class Player{
+  final int playerID;
+  final String name;
+  int points = 0;
+  List<Selection> myPredictions = [];
+  int indexValue = -1;
+  int correct = 0;
+  int wrong = 0;
+
+  Player(this.playerID, this.name);
+
+  // Enter new Prediction
+  void enterNewPrediction(Selection newSelection){
+    myPredictions.add(newSelection);
+  }
+
+  // Update points tally
+  void updateTally(){
+    if(myPredictions.isNotEmpty && (indexValue<myPredictions.length)){
+      // Calculate Score
+      for (int i=indexValue+1; i<myPredictions.length; i++){
+        Selection index = myPredictions[i];
+        if(index.winOrLose == 1){
+          correct++;
+          points+=2;
+          indexValue++;
+        }else if(index.winOrLose == 0){
+          wrong++;
+          indexValue++;
+        }
+      }
+    }
+  }
+}
--- lib/model/selection.dart
+++ lib/model/selection.dart
@@ -2,6 +2,8 @@ class Selection{
   final int selectionID;
   final int playerID;
   final int predictionID;
+  int winOrLose = -1; // 0 for loss, 1 for win, -1 for not completed yet
+  DateTime? date; 
 
-  Selection({required this.selectionID, required this.playerID, required this.predictionID});
+  Selection({required this.selectionID, required this.playerID, required this.predictionID, this.date});
 }
