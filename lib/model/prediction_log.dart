af92268b6b996e887d23b981acd90a87d5c947cf
CdeJohane <j.r.t.johane@gmail.com>
2024-08-16 15:13:15 +0200

    Modified Quicksort Algorithm

--- lib/model/prediction_log.dart
+++ lib/model/prediction_log.dart
@@ -6,26 +6,58 @@ class PredictionLog{
   PredictionLog(this.standings);
 
   // Quicksort to sort the positions
-  void sortPositions({int? lo, int? hi}){
+  /*void sortPositions({int? lo, int? hi}){
     lo ??= 0;
     hi ??= standings.length-1;
 
     // Commence the split and sort
     Player pivotPlayer = standings[hi];
-    int i = lo -1;
+    int i = lo;
     for (int a = lo; a<hi; a++){
       if (standings[a].points.compareTo(pivotPlayer.points) > 0){
+        Player temp = standings[i];
+        standings[i] = standings[a];
+        standings[a] = temp;
         i++;
-        Player temp = standings[i+1];
-        standings[i+1] = standings[hi];
-        standings[hi] = temp;
       }
     }
-    Player temp = standings[i + 1];
-    standings[i + 1] = standings[hi];
+    Player temp = standings[i];
+    standings[i] = standings[hi];
     standings[hi] = temp;
+
+    int pi = i;
     // return i+1
-    sortPositions(lo: lo, hi: i);
-    sortPositions(lo: i+2, hi: hi);
+    sortPositions(lo: lo, hi: pi-1);
+    sortPositions(lo: pi+1, hi: hi);
+  }*/
+
+  void sortPositions({int? lo, int? hi}) {
+  lo ??= 0;
+  hi ??= standings.length - 1;
+
+  if (lo < hi) {
+    int pi = partition(lo, hi);
+    sortPositions(lo: lo, hi: pi - 1);
+    sortPositions(lo: pi + 1, hi: hi);
   }
+}
+
+int partition(int low, int high) {
+  Player pivot = standings[high];
+  int i = low;
+
+  for (int j = low; j < high; j++) {
+    if (standings[j].points.compareTo(pivot.points) > 0) {
+      Player temp = standings[i];
+      standings[i] = standings[j];
+      standings[j] = temp;
+      i++;
+    }
+  }
+
+  Player temp = standings[i];
+  standings[i] = standings[high];
+  standings[high] = temp;
+  return i;
+}
 }
